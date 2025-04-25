7141e9f533bdf4a3f748ebe7e3c15170a92c5808
Green <ivan_koyrushki@abv.bg>
2025-04-25 18:17:53 +0300

    BossArena_2
    Added gate script background and light effect, changed the colliders for the moss by moving it into another tilemap

---
+++ Assets/Scripts/Mush/BossGate.cs
@@ -0,0 +1,68 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.Rendering.Universal;
+using System.Collections.Generic;
+public class BossGate : MonoBehaviour
+{
+    [SerializeField]private float moveSpeed;
+    [SerializeField]private float lightSpeed;
+    [SerializeField]private Tilemap gate;
+    [SerializeField] private GameObject layer;
+    private bool startMoving = false;
+    private List<Light2D> lights;
+
+    //Turns the lights off and make their intensity to 0 
+    private void Start()
+    {
+        lights = new List<Light2D>();
+        for (int i =0; i<layer.transform.childCount; i++)
+        {
+            Light2D item = layer.transform.GetChild(i).GetComponent<Light2D>();
+            lights.Add(item);
+        }
+
+        foreach (Light2D item in lights)
+        {
+            item.intensity = 0f;
+        }
+        
+        layer.SetActive(false);
+    }
+
+    void Update()
+    {
+        //moves the gate
+        if (startMoving && gate.transform.localPosition.y < 0f)
+        {
+            Move(gate);
+            layer.SetActive(true);
+        }
+        
+        //slowly rises the light's intensity
+        if (startMoving && lights[0].intensity < 1 )
+        {
+            foreach (Light2D item in lights)
+            {
+                item.intensity += lightSpeed;
+                
+            }
+        }
+    }
+    
+    //registers that we have triggered the zone
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        startMoving = true;
+    }
+
+    //moves the gate
+    public void Move(Tilemap tilemap)
+    {
+        Vector3 newPos = tilemap.transform.localPosition;
+        newPos.y += moveSpeed;
+ 
+        tilemap.transform.localPosition = newPos;
+    }
+}
